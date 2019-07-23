ddf33fef70203507b44653092ca8bb77cfc066c0
Ian <ismoore@mix.wvu.edu>
2019-07-23 14:37:40 -0400

    weathermap functionality, map zoom to location

--- app.js
+++ app.js
@@ -10,8 +10,21 @@ var zipcode;
 var baseWeatherUrl = 'https://api.openweathermap.org/data/2.5/weather?zip=';
 var endWeatherUrl = '&units=imperial&appid=df2de3900e635249f5651233e62fd47c';
 
-// displays an openlayers map
-var map = new ol.Map({
+// used with addWeatherInfo function
+var tempData;
+var forecastData;   
+var cloudData;
+var weatherData;
+var lon;
+var lat;
+
+// the map that will be used to display weathermaps
+var map;
+
+// renders an openlayers base map
+function addBackgroundMap() {
+  // displays an openlayers map
+    map = new ol.Map({
     target: 'map',
     layers: [
       new ol.layer.Tile({
@@ -24,39 +37,73 @@ var map = new ol.Map({
     })
   });
 
-  // adds the openweathermap layer to the map
+  
+}
+
+// adds a weather map over the base map
+function addWeatherMap(weatherType) {
+
+  // check if a weathermap layer already exists, if so, remove it
+  if (map.getLayers().getLength() > 1) {
+    map.getLayers().pop();
+  }
+
+  // add the weathermap layer
   var weather = new ol.layer.Tile({
-      source: new ol.source.XYZ({
-          url: baseMapUrl + precipitationNew + endMapUrl
-      })
+    source: new ol.source.XYZ({
+        url: baseMapUrl + weatherType + endMapUrl
+    })
   });
   map.addLayer(weather);
+}
 
 
-var tempData;
-var forecastData;   
-var cloudData;
-var weatherData;
+// sends query to openweathermap and then displays the returned info
+function addWeatherInfo() {
 
+   // get the zip
+   weatherUrl = baseWeatherUrl + document.getElementById('zip_txt').value + endWeatherUrl;
+
+   // make JSON object that will be filled with weather data after location is picked
+ $.getJSON(weatherUrl, function(data) {
+  
+  weatherData = data;
+  tempData = weatherData.main.temp;
+  forecastData = weatherData.weather[0].main;
+  cloudData = weatherData.weather[0].description;
+  lon = weatherData.coord.lon;
+  lat = weatherData.coord.lat;
+ })
+
+ if (weatherData) {
+   document.getElementById('temp').innerHTML = tempData + '°' + 'F';
+   document.getElementById('weather').innerHTML = forecastData;
+   document.getElementById('cloud-cover').innerHTML = cloudData;
+   map.getView().setCenter(ol.proj.fromLonLat([lon, lat])); // sets the center of the map to the user entered zip code(lon/lat)
+}
+}
+
+
+function main() {
+  addBackgroundMap();
+  
+  document.getElementById('temp_new').onclick = function() {
+		addWeatherMap("temp_new");
+  };
+  
+  document.getElementById('precipitation_new').onclick = function() {
+		addWeatherMap("precipitation_new");
+  };
+  
+  document.getElementById('clouds_new').onclick = function() {
+		addWeatherMap("clouds_new");
+	};
 
   // gets the zipcode and uses it to send a query for the location's weather
-document.getElementById('zip_button').onclick = function() {
-    
-    // get the zip, send query and display
-    weatherUrl = baseWeatherUrl + document.getElementById('zip_txt').value + endWeatherUrl;
+  document.getElementById('zip_button').onclick = function() {
+    addWeatherInfo();
+  };
 
-    // make JSON object that will be filled with weather data after location is picked
-  $.getJSON(weatherUrl, function(data) {
-      
-    weatherData = data;
-    tempData = weatherData.main.temp;
-    forecastData = weatherData.weather[0].main;
-    cloudData = weatherData.weather[0].description;
-  })
+}
 
-  if (weatherData) {
-    document.getElementById('temp').innerHTML = tempData + '°' + 'F';
-    document.getElementById('weather').innerHTML = forecastData;
-    document.getElementById('cloud-cover').innerHTML = cloudData;
- }
-};
+main();
